68a5bf0ed9016913fcf961f60f0f54c7ac7f62c4
unknown <neetamourya24@gmail.com>
2021-12-20 15:04:47 +0530

    [Neeta mourya] edit contact in UC3 branch

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookSystem.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookSystem.java
@@ -14,6 +14,7 @@ public class AddressBookSystem {
         System.out.println(" 0. Exit. ");
         System.out.println(" 1. Create contact. ");
         System.out.println(" 2. Add contact. ");
+        System.out.println(" 3. Edit contact. ");
         menu = sc.nextInt();
 
         while (menu != 0) {
@@ -63,10 +64,17 @@ public class AddressBookSystem {
                     menu = sc.nextInt();
                 }
                 break;
+
+            case 3:
+                System.out.println("Enter First Name of contact that you would like to edit: ");
+                int index = sc.nextInt();
+                System.out.println("Updated name is: ");
+                break;
             }
             System.out.println(" 0. Exit. ");
             System.out.println(" 1. Create contact. ");
             System.out.println(" 2. Add contact. ");
+            System.out.println(" 3. Edit contact. ");
             menu = sc.nextInt();
         }
         System.out.println("Goodbye!");
@@ -82,4 +90,13 @@ public class AddressBookSystem {
     	Contact person = new Contact(firstName, lastName, address , city, state, zip, phoneNumber, email);
         contact.add(person);
         }
+    private void editData(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNumber, String email) {
+    	Contact person = new Contact(firstName, lastName, address , city, state, zip, phoneNumber, email);
+        	 boolean index;
+			if (index = contact != null) {
+             	boolean temp=index;
+        	return;
+        }
+    }
 }
